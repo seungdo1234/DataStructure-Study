e649d3b31e03139ebd26099d3e233c542f288368
seungdo1234 <wltmdeh12@naver.com>
2022-09-07 10:15:09 +0900

    백준 2751번 문제

---
+++ DataStructure/Sort(Stack).cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int length,value;
+	
+	cin >> length;
+
+	int* arr = new int[length];
+
+	for (int i = 0; i < length; i++) {
+		cin >> value;
+		arr[i] = value;
+	}
+	
+	
+	sort(arr, arr + length);
+	
+	for (int i = 0; i < length; i++) {
+		cout << arr[i] << "\n";
+	}
+
+	delete[] arr;
+	return 0;
+}
